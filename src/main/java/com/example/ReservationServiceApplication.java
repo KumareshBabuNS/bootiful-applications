51dc0646b3a5e181072f24b74f114f21f7046e97
Neven Cvetkovic <nevenc@pivotal.io>
2016-08-01 00:40:41 +0200

    Added HealthIndicator.

--- src/main/java/com/example/ReservationServiceApplication.java
+++ src/main/java/com/example/ReservationServiceApplication.java
@@ -1,13 +1,21 @@
 package com.example;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ReservationServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReservationServiceApplication.class, args);
+	}
+
+	@Bean
+	HealthIndicator myIndicator() {
+		return () -> Health.status("I love Spring Boot!").build();
 	}
 
 }
