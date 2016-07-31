7daab5fdddb4df9f9ac4e5a2a92810bc51b368e6
Neven Cvetkovic <nevenc@pivotal.io>
2016-08-01 00:31:24 +0200

    Refactored to RestRepository using @RepositoryRestResource

--- src/main/java/com/example/ReservationRepository.java
+++ src/main/java/com/example/ReservationRepository.java
@@ -1,6 +1,8 @@
 package com.example;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ReservationRepository extends JpaRepository<Reservation,Long>{
 }
