d3904b2d09f0f496e65d4a19decf2fb3b4eec402
Neven Cvetkovic <nevenc@pivotal.io>
2016-08-01 00:35:02 +0200

    Added few finders to the RestRepository.

--- src/main/java/com/example/ReservationRepository.java
+++ src/main/java/com/example/ReservationRepository.java
@@ -1,8 +1,16 @@
 package com.example;
 
+import java.util.Collection;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 @RepositoryRestResource
 public interface ReservationRepository extends JpaRepository<Reservation,Long>{
+	@RestResource(path="find-by-rn")
+	Collection<Reservation> findByReservationName(@Param("rn") String rn);
+	@RestResource(path="find-by-rn-containing")
+	Collection<Reservation> findByReservationNameContainingIgnoreCase(@Param("rn") String rn);
 }
